ba46fcc723f69fa70aa84fd049123f873dffee32
guofeng zhang <guofeng.zhang@mybean.cn>
2018-01-14 19:41:04 +0800

    save to db

--- src/main/resources/script-cleanup.sql
+++ src/main/resources/script-cleanup.sql
@@ -1,5 +1,5 @@
-REVOKE * ON crawler.* FROM 'crawler’@'localhost';
-REVOKE * ON crawler.* FROM 'crawler’@'%';
+REVOKE ALL PRIVILEGES ON crawler.* FROM 'crawler'@'localhost';
+REVOKE ALL PRIVILEGES ON crawler.* FROM 'crawler'@'%';
 
 DROP USER 'crawler'@'localhost';
 DROP USER 'crawler'@'%';
--- src/main/resources/script-setup.sql
+++ src/main/resources/script-setup.sql
@@ -28,3 +28,45 @@ CREATE TABLE crawler_data_cn (
     Product_restock_Date Date,
     PRIMARY KEY (id)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+CREATE TABLE crawler_data_hk (
+    id   bigint(32) NOT NULL AUTO_INCREMENT,
+    code VARCHAR(30) COMMENT '',
+    title      VARCHAR(255),
+    details    VARCHAR(30),
+    price      decimal(10,2),
+    sizes      VARCHAR(255),
+    product_url VARCHAR(255),
+    Product_Live VARCHAR(30),
+    Product_Live_Date Date,
+    Product_Soldout_Date Date,
+    Product_Broken_Size VARCHAR(255),
+    Product_Last_Broken_Size VARCHAR(255),
+    Product_Broken_Size_Date Date,
+    sale_off_rate decimal(6,2),
+    sale_off_rate_date Date,
+    Product_restock VARCHAR(255),
+    Product_restock_Date Date,
+    PRIMARY KEY (id)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+CREATE TABLE crawler_data_us (
+    id   bigint(32) NOT NULL AUTO_INCREMENT,
+    code VARCHAR(30) COMMENT '',
+    title      VARCHAR(255),
+    details    VARCHAR(30),
+    price      decimal(10,2),
+    sizes      VARCHAR(255),
+    product_url VARCHAR(255),
+    Product_Live VARCHAR(30),
+    Product_Live_Date Date,
+    Product_Soldout_Date Date,
+    Product_Broken_Size VARCHAR(255),
+    Product_Last_Broken_Size VARCHAR(255),
+    Product_Broken_Size_Date Date,
+    sale_off_rate decimal(6,2),
+    sale_off_rate_date Date,
+    Product_restock VARCHAR(255),
+    Product_restock_Date Date,
+    PRIMARY KEY (id)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
