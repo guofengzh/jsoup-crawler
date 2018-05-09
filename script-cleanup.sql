31f804e99b18835853b171b16d269d6dc9b8ce60
guofeng <guofengzh@gmail.com>
2018-05-09 15:23:29 +0800

    add brands and daily records

--- script-cleanup.sql
+++
@@ -1,7 +0,0 @@
-REVOKE ALL PRIVILEGES ON crawler.* FROM 'crawler'@'localhost';
-REVOKE ALL PRIVILEGES ON crawler.* FROM 'crawler'@'%';
-
-DROP USER 'crawler'@'localhost';
-DROP USER 'crawler'@'%';
-
-DROP DATABASE crawler;
